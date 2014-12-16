192ed56c37d89d1b2520edfaa5df8699da80f9f0
soumenganguly <soumendotganguly@gmail.com>
2014-12-17 01:33:58 +0530

    minor modification to notify.sh

--- battery/notify.sh
+++ battery/notify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 power=$(cat power.txt)
-hours=`expr power / 60`
-mins=`expr power % 60`
+hours=`expr $power / 60`
+mins=`expr $power % 60`
 notify-send "echo '$hours hours $mins mins. remaining"
